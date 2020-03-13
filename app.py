f35f948e10c4a7bd2cc4e1081356a3d0adb22e80
miraz <mazharul.miraz@outlook.com>
2020-03-13 13:44:37 +0530

    debug routing path

--- app.py
+++ app.py
@@ -4,24 +4,37 @@ from flask import Flask, render_template, url_for, request, redirect , send_from
 app = Flask(__name__)
 
 @app.route("/")
-def approot():
+def viewpage():
     return  render_template('index.html')
 
 @app.route("/login")
-def approot():
+def login():
     return  render_template('login.html')
 
 @app.route("/search")
-def approot():
+def search():
     return  render_template('search.html')
 
-@app.route("/")
-def approot():
-    return  render_template('index.html')
+@app.route("/crt")
+def create():
+    return  render_template('crt.html')
 
-@app.route("/")
-def approot():
-    return  render_template('index.html')
+
+@app.route("/allrec")
+def allrec():
+    return  render_template('allrec.html')
+
+
+
+# Error Handlers
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+def create_app(config_filename):
+    app = Flask(__name__)
+    app.register_error_handler(404, page_not_found)
+    return app
+
 
 
 
